cb93beb740fddd5ff9558d9e684ef2ba5f32c954
Paula Cavalcante <paulacavalcante@gmail.com>
2018-02-02 14:18:22 -0200

    logica da aprovacao

--- src/providers/noe/noe.ts
+++ src/providers/noe/noe.ts
@@ -10,10 +10,46 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class NoeProvider {
   itens = [];
+  orcamentos = [
+    {
+      titulo:"",
+      opcoes: []  
+    }
+  ];
+
+  // orcamentos[i]
 
   inserirItensServico(item){
-   this.itens.push(item);
-   console.log(this.itens);  
- }
 
-}
+    this.itens.push(item);
+    console.log("itens =");
+    console.log(this.itens);
+  
+    let tipoExistente = false;
+    
+    for (let i = 0; i < this.orcamentos.length; i++) {
+  
+      if (this.itens[this.itens.length-1].tipo === this.orcamentos[i].titulo){
+        this.orcamentos[i].opcoes.push(this.itens[this.itens.length-1]); 
+        tipoExistente = true;
+      }
+    }
+  
+    if ((tipoExistente === false) && (this.orcamentos[0].titulo !== "")){
+      this.orcamentos.push({titulo: "", opcoes: []});      
+      this.orcamentos[this.orcamentos.length-1].opcoes.push(this.itens[this.itens.length-1]);
+      this.orcamentos[this.orcamentos.length-1].titulo = this.itens[this.itens.length-1].tipo;
+      }
+    
+    else if ((tipoExistente === false) && (this.orcamentos[0].titulo === "")){
+      this.orcamentos[0].opcoes.push(this.itens[this.itens.length-1]);
+      this.orcamentos[0].titulo = this.itens[this.itens.length-1].tipo;
+    }
+    console.log("orcamentos =");
+    console.log(this.orcamentos)
+  }
+  
+  }
+  
+
+
